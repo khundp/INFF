9ae952452037913e23e69c60da411623f6a3f810
Knowledge hub UNDP Nigeria <93338224+khundp@users.noreply.github.com>
2026-04-27 12:49:59 +0300

    Add files via upload

--- js/data/byYear.js
+++ js/data/byYear.js
@@ -11,7 +11,8 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 3.9,
     "priv": 3.9,
     "ip": 3.6,
-    "ipriv": 2.2
+    "ipriv": 2.2,
+    "subnationalGovernmentRevenues": {"igr":1.2,"stateTaxesLevies":0.9,"feesCharges":0.6,"propertyRates":0.4,"localGovernmentRevenues":0.8}
   },
   "2021": {
     "year": 2021,
@@ -23,7 +24,8 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 4.4,
     "priv": 4.6,
     "ip": 4.0,
-    "ipriv": 2.4
+    "ipriv": 2.4,
+    "subnationalGovernmentRevenues": {"igr":1.4,"stateTaxesLevies":1.0,"feesCharges":0.7,"propertyRates":0.5,"localGovernmentRevenues":0.9}
   },
   "2022": {
     "year": 2022,
@@ -35,7 +37,8 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 4.9,
     "priv": 5.1,
     "ip": 4.5,
-    "ipriv": 2.7
+    "ipriv": 2.7,
+    "subnationalGovernmentRevenues": {"igr":1.6,"stateTaxesLevies":1.1,"feesCharges":0.8,"propertyRates":0.6,"localGovernmentRevenues":1.0}
   },
   "2023": {
     "year": 2023,
@@ -47,7 +50,8 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 9.2,
     "priv": 9.1,
     "ip": 8.4,
-    "ipriv": 5.0
+    "ipriv": 5.0,
+    "subnationalGovernmentRevenues": {"igr":3.0,"stateTaxesLevies":2.1,"feesCharges":1.4,"propertyRates":0.9,"localGovernmentRevenues":1.8}
   },
   "2024": {
     "year": 2024,
@@ -59,7 +63,8 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 18.2,
     "priv": 18.2,
     "ip": 16.2,
-    "ipriv": 9.6
+    "ipriv": 9.6,
+    "subnationalGovernmentRevenues": {"igr":5.8,"stateTaxesLevies":4.1,"feesCharges":2.7,"propertyRates":1.8,"localGovernmentRevenues":3.8}
   },
   "2025": {
     "year": 2025,
@@ -71,6 +76,7 @@ window.INFF_DATA_PARTS.byYear = {
     "dp": 20.9,
     "priv": 21.2,
     "ip": 18.8,
-    "ipriv": 11.6
+    "ipriv": 11.6,
+    "subnationalGovernmentRevenues": {"igr":6.7,"stateTaxesLevies":4.7,"feesCharges":3.1,"propertyRates":2.0,"localGovernmentRevenues":4.4}
   }
 };
